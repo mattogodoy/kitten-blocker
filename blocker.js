ece43924929ee400c6cec3f786a6d943636d5182
Matto <macmini2@Mac-mini.local>
2014-10-30 16:33:26 +0100

    master: First commit.

---
+++ blocker.js
@@ -0,0 +1,54 @@
+function Randomize(images) {
+  return Math.floor(Math.random() * images.length)
+}
+
+function imageRatio(image) {
+  var proportion = image.height / image.width;
+
+  if (proportion > 1) {
+    return "vertical";
+  } else if (proportion === 1) {
+    return "square";
+  } else if (proportion < 1) {
+    return "horizontal";
+  }
+}
+
+var matchString = function(string) {
+  if (!string) {
+    return false;
+  }
+
+  var kWords = [
+    "gato",
+    "gatito",
+    "cat",
+    "kitty",
+    "kitten"
+  ];
+  var re = new RegExp(kWords.join('|'), "gi");
+
+  if (string.search(re) < 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+var findCats = function(image) {
+  if (matchString(image.src) || matchString(image.alt) || matchString(image.title)) {
+    console.log("####### Fucking cats everywhere!!");
+    image.src = chrome.extension.getURL('/img/rick-astley.jpg');
+  }
+
+// console.log(chrome.extension.getURL('/img/rick-astley.jpg'));
+};
+
+(function (document) {
+  var images = document.getElementsByTagName('img');
+  var length = images.length;
+
+  for (var i = 0; i < length; i++) {
+    findCats(images[i]);
+  }
+})(document);
